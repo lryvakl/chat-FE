3f5a14f08c695d62060bccff8972a74b8f903f63
Valeriia Ryvak <leraruvak2006@gmail.com>
2025-12-15 21:56:56 +0200

    fix: problem witch auth store cycle

--- src/api/axios.instance.ts
+++ src/api/axios.instance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { SERVER_URL } from "../constants/index";
-import { store } from "../store/index";
+import type { AppStore } from "../store/index";
 import { logout } from "../store/authSlice";
 
 const http = axios.create({
@@ -8,25 +8,26 @@ const http = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-http.interceptors.request.use((config) => {
-  const state = store.getState();
-  const token = state.auth.token;
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+export const setupAxiosInterceptors = (store: AppStore) => {
+  http.interceptors.request.use((config) => {
+    const token = store.getState().auth.token;
 
-  return config;
-});
-
-http.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      console.warn("Token expired or invalid");
-      store.dispatch(logout());
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return Promise.reject(error);
-  }
-);
 
+    return config;
+  });
+
+  http.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.status === 401) {
+        console.warn("Token expired or invalid");
+        store.dispatch(logout());
+      }
+      return Promise.reject(error);
+    }
+  );
+};
 export default http;
