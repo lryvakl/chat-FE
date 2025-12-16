37fa4a556995f8e8bf51a0851e40d91cd25e10d1
Valeriia Ryvak <leraruvak2006@gmail.com>
2025-12-16 11:53:31 +0200

    refactor: simplify routing logic with ProtectedRoute and GuestRoute components

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
 import ChatPage from "./pages/ChatPage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import { useSelector } from "react-redux";
 import type { RootState } from "./store";
 import "./App.css";
 
+const ProtectedRoute = () => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.token);
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+const GuestRoute = () => {
+  const isAuthenticated = useSelector((state: RootState) => state.auth.token);
+  return isAuthenticated ? <Navigate to="/chat/General" replace /> : <Outlet />;
+};
+
 function App() {
-  const token = useSelector((state: RootState) => state.auth.token);
   return (
     <Routes>
-      <Route
-        path="/login"
-        element={!token ? <LoginPage /> : <Navigate to="/chat/General" />}
-      />
-      <Route
-        path="/register"
-        element={!token ? <RegisterPage /> : <Navigate to="/chat/General" />}
-      />
-
-      <Route
-        path="/chat/:room"
-        element={token ? <ChatPage /> : <Navigate to="/login" />}
-      />
-
-      <Route path="/chat" element={<Navigate to="/chat/General" replace />} />
-
-      <Route path="/" element={<Navigate to="/chat/General" />} />
+      <Route element={<GuestRoute />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/chat/:room" element={<ChatPage />} />
+        <Route path="/chat" element={<Navigate to="/chat/General" replace />} />
+      </Route>
+      <Route path="/" element={<Navigate to="/chat/General" replace />} />
     </Routes>
   );
 }
